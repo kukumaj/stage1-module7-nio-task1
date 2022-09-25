4e9d40eb4647578d2fffd7cedc177c18b255fb26
kukumaj <zakumaj@gmail.com>
2022-09-25 09:19:08 +0200

    done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.nio;
 
+import javax.imageio.IIOException;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+            Path path = file.toPath();
+        try (var reader = Files.newBufferedReader(path)) {
+            for (String line = reader.readLine();line != null;line = reader.readLine()) {
+                var entry = line.split(": ");
+                var key = entry[0];
+                var value = entry[1];
+                switch (key) {
+                    case "Name":
+                        profile.setName(value);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.valueOf(value));
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.valueOf(value));
+                        break;
+                    case "Email":
+                        profile.setEmail(value);
+                        break;
+                }
+            }
+            return profile;
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+        }
     }
 }
